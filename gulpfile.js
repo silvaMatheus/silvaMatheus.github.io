4a8ddee952f4dc5442655953d1a0f2a15ae9f6bb
Matheus <ms190698@gmail.com>
2019-03-02 13:54:31 -0300

    site em produção

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,22 @@ var gulp = require('gulp'); // Chamando o Gulp
 
 //Tasks  - Funções que o gulp desempenhará
 
-
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 
 //Tasks  para o sass
 
 gulp.task('sass', function () {
+  console.log("\n Sass compilado \n");
     return gulp.src('sass/*.scss')
     .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest('css'));
 });
 
 //task para o watch
-	gulp.task('watch', function () {
-    console.log("funcionando");
-   	gulp.watch('sass/*.scss', ['sass']);
+gulp.task('watch', function () {
+    console.log("\n Arquivo sendo monitorado... \n");
+   	gulp.watch('sass/*.scss', gulp.series('sass'));
 	});
 
-gulp.task('default', function(){
-
-});
+gulp.task('default', gulp.series( 'sass', 'watch'));
